6ec92dc33389cc0de3636937daf0784a23bfc3e8
mars-1002 <matthewabsalonramirez@gmail.com>
2023-06-07 18:03:26 -0400

    add login/signup func

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('memes/', views.meme_index, name='meme-index'),
   path('memes/<int:meme_id>/', views.meme_detail, name='meme-detail'),
   path('memes/create/', views.MemeCreate.as_view(), name='meme-create'),
   path('memes/<int:pk>/update/', views.MemeUpdate.as_view(), name='meme-update'),
   path('memes/<int:pk>/delete/', views.MemeDelete.as_view(), name='meme-delete'),
-  path('memes/canvas/', views.meme_canvas, name='meme-canvas')
+  path('memes/canvas/', views.meme_canvas, name='meme-canvas'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Meme
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 import uuid
 import boto3
 
@@ -8,33 +13,53 @@ import boto3
 from django.http import HttpResponse
 
 # Define the home view
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def meme_index(request):
   memes = Meme.objects.all()
   return render(request, 'memes/index.html', { 'memes': memes })
 
+@login_required
 def meme_detail(request, meme_id):
   meme = Meme.objects.get(id=meme_id)
   return render(request, 'memes/detail.html', {'meme': meme})
 
-class MemeCreate(CreateView):
+class MemeCreate(LoginRequiredMixin, CreateView):
   model = Meme
   fields = '__all__'
   success_url = '/memes/'
 
-class MemeUpdate(UpdateView):
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class MemeUpdate(LoginRequiredMixin, UpdateView):
   model = Meme
   # Let's disallowmthe renaming of a Meme by excluding the name field!
   fields = '__all__'
 
-class MemeDelete(DeleteView):
+class MemeDelete(LoginRequiredMixin, DeleteView):
   model = Meme
   success_url = '/memes/'
 
 def meme_canvas(request):
   return render(request, 'canvas.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('meme-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)

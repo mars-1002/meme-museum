a105188437704a77865eec66f19099149ebb4a43
mars-1002 <matthewabsalonramirez@gmail.com>
2023-06-08 21:39:51 -0400

    add conditionals and permission check

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-Work in progress
+# [Click here to check out Meme Museum!](https://meme-museum.fly.dev/)
+
+## Getting Started
+
+## Highlight
+<p>Meme Museum uses jquery to add a event listener to a HTML canvas. This feature allows the user to dynamically create a new meme only using HTML + JS.</p>
+
+```
+function handleLoadImg () {
+    context.drawImage(image, 0, 0, 500, 300)
+    image.removeEventListener('load', handleLoadImg)
+}
+
+function genImage () {
+    $.ajax('https://dog.ceo/api/breed/shiba/images/random').done(function(data) {
+    shibeImage = data.message
+    image.src=shibeImage
+    image.addEventListener('load', handleLoadImg)
+})
+}
+```
+
+## Technologies Used
+- HTML
+- CSS
+- Python
+- JavaScript
+- AJAX
+- JQuery
+- Django
+- Bit.io
+- AWS
+- Docker
+- Photoshop CS 2023
+- Whimsical
+
+## ERD
+![Meme Museum ERD](./main_app/static/assets/ERD.png)
+
+## IceBox
+
+## Attributions
+- [Dog.ceo Dog API](https://dog.ceo/dog-api/)
+- [bit.io SQL Database](https://bit.io/)
+- [fly.io App Deployment](https://fly.io/)
+- [Whimsical](https://whimsical.com/)
+- [Amazon AWS S3](https://aws.amazon.com/s3/)
--- main_app/static/js/canvas.js
+++ main_app/static/js/canvas.js
@@ -4,6 +4,8 @@ var context = canvas.getContext('2d')
 var image = document.getElementById('shibe')
 image.style.display = 'none' //hides the image src
 var shibeImage 
+
+
 var newDogeBtn = document.getElementById("newDoge")
 newDogeBtn.addEventListener("click", genImage)
 var memeText = document.getElementById('changeText').value
@@ -17,9 +19,11 @@ context.fillStyle = colorIndex[colorRan]
 
 // functions to gen new image from dog.ceo API
 function handleLoadImg () {
+    context.font = "bold 30px Comic Neue"
     context.drawImage(image, 0, 0, 500, 300)
     image.removeEventListener('load', handleLoadImg)
 }
+
 function genImage () {
     $.ajax('https://dog.ceo/api/breed/shiba/images/random').done(function(data) {
     shibeImage = data.message
@@ -35,7 +39,11 @@ newText.addEventListener('click', newColorGen)
 function newColorGen () {
     context.fillStyle = colorIndex[Math.floor(Math.random() * (3))]
 }
+
 function changeMemeText () {  
     memeText = document.getElementById('changeText').value
     context.fillText(memeText, Math.random() * (100) + 50, Math.random() * (150) + 50) //src txt, x, y
+    
 }
+
+changeMemeText()
